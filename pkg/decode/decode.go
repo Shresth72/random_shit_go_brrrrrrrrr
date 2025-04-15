ab2edcbdacab62e7c64f64f79b4af92222e82c78
Shresth72 <97455610+Shresth72@users.noreply.github.com>
2025-04-15 16:21:42 +0000

    integer parsing, fix edge cases

--- pkg/decode/decode.go
+++ pkg/decode/decode.go
@@ -8,22 +8,50 @@ import (
 )
 
 func DecodeBencodedValue(encodedValue string) (interface{}, error) {
-	if len(encodedValue) > 0 && unicode.IsDigit(rune(encodedValue[0])) {
-		splits := strings.SplitN(encodedValue, ":", 2)
-		if len(splits) != 2 {
-			return nil, fmt.Errorf("Malformed bencode string")
-		}
-
-		strLen, err := strconv.Atoi(splits[0])
-		if err != nil {
-			return nil, err
-		}
-
-		if len(splits[1]) < strLen {
-			return nil, fmt.Errorf("Not enough characters for the string")
-		}
-		decodedString := splits[1][:strLen]
-		return decodedString, nil
+	if len(encodedValue) == 0 {
+		return nil, fmt.Errorf("Empty encoded value")
 	}
+
+	switch encodedValue[0] {
+	case 'i':
+		return decodeInteger(encodedValue)
+	default:
+		if unicode.IsDigit(rune(encodedValue[0])) {
+			return decodeString(encodedValue)
+		}
+	}
+
 	return nil, fmt.Errorf("Unhandled encoded value: %s", encodedValue)
+}
+
+func decodeInteger(encodedValue string) (int, error) {
+	if !strings.Contains(encodedValue, "e") {
+		return 0, fmt.Errorf("Invalid integer encoding: missing 'e' terminator")
+	}
+
+	encodedValue = encodedValue[1:]
+	decoded, err := strconv.Atoi(strings.SplitN(encodedValue, "e", 2)[0])
+	if err != nil {
+		return 0, err
+	}
+
+	return decoded, nil
+}
+
+func decodeString(encodedValue string) (string, error) {
+	splits := strings.SplitN(encodedValue, ":", 2)
+	if len(splits) != 2 {
+		return "", fmt.Errorf("Malformed bencode string")
+	}
+
+	strLen, err := strconv.Atoi(splits[0])
+	if err != nil {
+		return "", err
+	}
+
+	if len(splits[1]) < strLen {
+		return "", fmt.Errorf("Not enough characters for the string")
+	}
+	decodedString := splits[1][:strLen]
+	return decodedString, nil
 }
--- pkg/decode/decode_test.go
+++ pkg/decode/decode_test.go
@@ -12,11 +12,29 @@ func TestDecodeBencodedValue(t *testing.T) {
 		expected      interface{}
 		expectedError bool
 	}{
+		// Integer parsing
+		{"i54e", 54, false},
+		{"isde", 0, true},
+		{"i0e", 0, false},
+		{"345e", 0, true},
+		{"i00e", 0, true},
+		{"i1a2e", 0, true},
+		{"i123ee", 0, true},
+		{"i-0e", 0, true},
+		{"ie", 0, true},
+		{"i54", 0, true},
+		{"i054e", 0, true},
+		{"i-10e", -10, false},
+		{"i54 e", 0, true},
+		{"i541235e235235", 541235, false},
+		// Strings parsing
 		{"4:spam", "spam", false},
 		{"0:", "", false},
 		{"7:Hello69", "Hello69", false},
 		{"", "", true},
 		{"5:Help", "", true},
+		{"5 :Hello", "", true},
+		{"0:extra", "", false},
 		{"abc:abc", "", true},
 		{"4spam", "", true},
 	}
@@ -24,7 +42,7 @@ func TestDecodeBencodedValue(t *testing.T) {
 	for _, test := range tests {
 		res, err := DecodeBencodedValue(test.input)
 		if test.expectedError {
-			assert.Error(t, err, "Expected error for input: %s", test.input)
+			assert.Error(t, err, "Expected error for input: %s and result: %v", test.input, res)
 		} else {
 			assert.NoError(t, err, "Unexpected error for input: %s", test.input)
 			assert.Equal(t, test.expected, res, "Mismatch for input: %s", test.input)
